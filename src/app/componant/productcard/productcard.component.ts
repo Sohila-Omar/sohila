a459cb662159a58b2ba8b402cec5336fd7c0dd2d
sohila <sohyla.abdelhamid@gamil.com>
2024-11-24 11:40:12 +0200

    video 3 part 3

--- src/app/componant/productcard/productcard.component.ts
+++ src/app/componant/productcard/productcard.component.ts
@@ -2,18 +2,19 @@ import { Component, Input } from '@angular/core';
 import { Iproduct } from '../../models/iproduct';
 import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 import { EGPcurrencyPipe } from '../../pipes/egpcurrency.pipe';
-import { AppComponent } from '../../app.component';
 import { HighlightDirective } from '../../dircetives/highlight.directive';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-productcard',
   standalone: true,
-  imports: [CurrencyPipe ,DatePipe, EGPcurrencyPipe,CommonModule,HighlightDirective],
+  imports: [CurrencyPipe ,DatePipe, EGPcurrencyPipe,CommonModule,HighlightDirective,RouterModule],
   templateUrl: './productcard.component.html',
   styleUrl: './productcard.component.css'
 })
 export class ProductcardComponent {
  @Input() dynamicProduct!:Iproduct;
+ taxes!: number;
 
 expireDate:Date =new Date();
 
---
+++ src/app/models/user.ts
@@ -0,0 +1,8 @@
+export class User {
+   
+
+ constructor( public userName: string,public age: number){
+    
+ }
+}
+const user1=new User('Sohila',20)
